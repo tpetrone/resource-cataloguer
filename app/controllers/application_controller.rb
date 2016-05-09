a784fd9f8d6356f07cef496ed8f96b19a98876e5
Kazuo256 <kazuo256@gmail.com>
2016-05-09 10:24:36 -0300

    Add index methods to BasicResource controller
    Signed off by: @Kazuo256 <kazuo256@gmail.com>
    Signed off by: @carolsatye <caroline.satye@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+  # GET /resources/sensors
+  def index_sensors
+    @basic_resources = BasicResources.where(sensor: true)
+    render json: @basic_resources
+  end
+
+  # GET /resources/actuators
+  def index_actuators
+    @basic_resources = BasicResources.where(actuator: true)
+    render json: @basic_resources
+  end
+
 end
