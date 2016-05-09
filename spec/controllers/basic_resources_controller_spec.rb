46c64679bb99240ebdb85ad870e12b0680466473
Kazuo256 <kazuo256@gmail.com>
2016-05-09 11:05:14 -0300

    Add rspec tests for controller
    Signed off by: @Kazuo256 <kazuo256@gmail.com>
    Signed off by: @carolsatye <caroline.satye@gmail.com>

--- spec/controllers/basic_resources_controller_spec.rb
+++ spec/controllers/basic_resources_controller_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe BasicResourcesController, :type => :controller do
 
-  #describe '.'
+  describe '#index_sensors' do
+
+    before :each do
+      get 'index_sensors', format: :json
+    end
+
+    it { expect(response.status).to eq(200) }
+
+    it 'is expected to return an empty JSON list' do
+      expect(response.body).to eq("[]")
+    end
+
+  end
 
 end
